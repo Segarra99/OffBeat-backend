e82c84ec0b4308abd022adcf9025ab9dd51ed126
Andre Pinto <andre.marpinto@gmail.com>
2023-08-31 19:33:29 +0100

    small corrections

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,13 +5,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.get("/contacts", (req, res, next) => {
-  res.json("Just a contacts page")
-})
 
-router.get("/about", (req, res, next) => {
-  res.json("What's the project about")
-})
 
 
 module.exports = router;
